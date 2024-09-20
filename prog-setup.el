d70a39a1932c9db480a9f9d83cc2bf968d3ab700
andyjda <bobodeangelis@gmail.com>
2024-09-20 19:09:53 -0400

    clean language-by-language setup

--- prog-setup.el
+++ prog-setup.el
@@ -20,23 +20,16 @@
 
 ;;; Commentary:
 
-;; 
-
 ;;; Code:
-;;;###autoload
-(defun prog-setup (&optional absolute)
-  (interactive "P")
-  (prog-setup-appearance)
-  (prog-setup-commands))
-
 ;;;; appearance
+;;;###autoload
 (defun prog-setup-appearance (&optional absolute)
-"Set preferred appearance for prog-mode"
-(display-line-numbers-mode t)
-(if absolute
-    (absolute-line-numbers-setup)
-  (relative-line-numbers-setup))
-(electric-pair-local-mode t))
+  "Set preferred appearance for prog-mode"
+  (display-line-numbers-mode t)
+  (if absolute
+      (absolute-line-numbers-setup)
+    (relative-line-numbers-setup))
+  (electric-pair-local-mode t))
 
 (defun adj/:around-goto-line-read-args (origfn)
   (let ((display-line-numbers 'absolute))
@@ -70,92 +63,29 @@
   (add-hook 'prog-mode-hook (lambda () (prog-setup 'absolute))))
 
 (defun relative-line-numbers-global ()
-(interactive)
-(line-numbers-global #'relative-line-numbers-setup)
-(remove-hook 'prog-mode-hook (lambda () (prog-setup 'absolute)))
-(add-hook 'prog-mode-hook #'prog-setup))
+  (interactive)
+  (line-numbers-global #'relative-line-numbers-setup)
+  (remove-hook 'prog-mode-hook (lambda () (prog-setup 'absolute)))
+  (add-hook 'prog-mode-hook #'prog-setup))
 
 ;;;; commands
-(defun prog-setup-commands ()
-  "Setup some custom commands for prog-mode"
-  (message "major-mode: %s" major-mode)
-
-  (setq lang-specific-run-this nil)
-  (make-local-variable 'lang-specific-run-this)
-
-  (when (setq lang-specific-run-this (prog-setup-get-func-run-this))
-    (progn
-      (message "lang-specific-run-this %s" lang-specific-run-this)
-      (message "binding it")
-      (local-set-key (kbd "C-c r") #'run-this)))
-  (message "here")
-
-  ;; TODO: generalize this logic:
-  ;; make a variable buffer local, then assign it to its lang-specific value
-  ;; TODO: write the lang-specific vars for javascript and rust
-  (setq lang-specific-format-quoted-thing nil)
-  (setq lang-specific-format-thing nil)
-  (setq lang-specific-format nil)
-  (make-local-variable 'lang-specific-format-quoted-thing)
-  (make-local-variable 'lang-specific-format-thing)
-  (make-local-variable 'lang-specific-format)
-  (setq lang-specific-format-quoted-thing #'python-format-thing)
-  (setq lang-specific-format-thing #'python-format-thing)
-  (setq lang-specific-format "%s(f\"%s : {%s}\")")
-  (setq lang-specific-action "print")
-  
-  (local-set-key (kbd "C-M-p") #'prog-setup-generic-debug)
-  ;; (local-set-key (kbd "C-M l") #'print-log)
-  (message "done"))
-
-;;;;; run-this
-(defun prog-setup-get-func-run-this ()
-  (pcase major-mode
-    ('python-mode #'python-run-this)
-    ('web-mode #'js-run-this)
-    ('emacs-lisp-mode #'elisp-eval-region-or-buffer)
-    ('json-mode nil) ;; ignore it
-    (_ (progn
-         (message
-          "prog-setup: this mode does not have a #'run-this implementation: %s"
-          major-mode)
-         nil))))
-
-(defun run-this ()
-  "Run the current program.
-Use the function specific in lang-specific-run-this"
-  (interactive)
-  (if (called-interactively-p 'any)
-      (call-interactively lang-specific-run-this)
-    (funcall lang-specific-run-this)))
-
-
 ;;;;; debug-statements
-(defun python-debug (action &optional arg)
-  (let* ((thing (if (or arg (current-line-empty-p))
-        	    (read-from-kill-ring (format "%s :" action))
-        	  (progn
-        	    (make-it-quiet (dwim-kill))
-        	    (pop kill-ring))))
-         (thing (string-replace "\"" "'" thing)))
-    (insert (format "%s(f\"%s : {%s}\")" action thing thing))))
-
-(defun python-format-thing (thing)
-  (string-replace "\"" "'" thing))
-
-(defun prog-setup-generic-debug ()
-  (interactive)
-  (let* ((thing (prog-setup-get-thing-to-print))
-         (named-thing (funcall lang-specific-format-quoted-thing thing))
-         (thing (funcall lang-specific-format-thing thing)))
-    (insert (format lang-specific-format lang-specific-action named-thing thing)))
-  )
-(defun prog-setup-get-thing-to-print ()
-  "Get the string to print.
-Use dwim-kill to get the current region or line."
-  (progn
-    (make-it-quiet (dwim-kill))
-    (pop kill-ring)))
+;;;###autoload
+(defun debug-print (arg &optional lang-format)
+  (interactive "P")
+  (let* ((lang-format (or lang-format local-lang-format
+                          ;; TODO make this an actual error that can be caught
+                          (error "no lang format provided")))
+         (thing (unless (current-line-empty-p)
+                  (progn
+                    (make-it-quiet (dwim-kill))
+                    (pop kill-ring))))
+         (line-number (when (or arg (not thing))
+                        (line-number-at-pos)))
+         ;; TODO add support to get the name of the current
+         ;; function/class/block
+         )
+    (insert (funcall lang-format thing line-number))))
 
 (provide 'prog-setup)
 
